54e8b6c0a3e5101576a84cce32f699c8e4d1e8d6
jubair-k <jubairk576@gmail.com>
2022-02-11 00:49:50 +0530

    admin dashboard product section

--- admin/connection.inc.php
+++ admin/connection.inc.php
@@ -1,4 +1,10 @@
 <?php
     session_start();
     $con=mysqli_connect("localhost:3308","root","","ecom");
+    define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/E-Commerce-website-using-PHP/');
+    define('SITE_PATH','http://localhost/E-Commerce-website-using-PHP/');
+
+    define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
+    define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
+
 ?>
