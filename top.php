3f7a0780e21a68c15cbc83f576153205dcc393b0
jubair-k <jubairk576@gmail.com>
2022-02-18 22:50:14 +0530

    change the title with product meta dscriptoon

--- top.php
+++ top.php
@@ -22,14 +22,33 @@ if(isset($_SESSION['USER_LOGIN'])){
 
 	$wishlist_count=mysqli_num_rows(mysqli_query($con,"select product.name,product.image,product.price,product.mrp,wishlist.id from product,wishlist where wishlist.product_id=product.id and wishlist.user_id='$uid'"));
 }
+
+$script_name=$_SERVER['SCRIPT_NAME'];
+$script_name_arr=explode('/',$script_name);
+$mypage=$script_name_arr[count($script_name_arr)-1];
+
+$meta_title="My Ecom Website";
+$meta_desc="My Ecom Website";
+$meta_keyword="My Ecom Website";
+if($mypage=='product.php'){
+	$product_id=get_safe_value($con,$_GET['id']);
+	$product_meta=mysqli_fetch_assoc(mysqli_query($con,"select * from product where id='$product_id'"));
+	$meta_title=$product_meta['meta_title'];
+	$meta_desc=$product_meta['meta_desc'];
+	$meta_keyword=$product_meta['meta_keyword'];
+}if($mypage=='contact.php'){
+	$meta_title='Contact Us';
+}
+
 ?>
 <!doctype html>
 <html class="no-js" lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Ecom Website</title>
-    <meta name="description" content="">
+    <title><?php echo $meta_title?></title>
+    <meta name="description" content="<?php echo $meta_desc?>">
+	<meta name="keywords" content="<?php echo $meta_keyword?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
